0494fed9ea4630396942ac1b437c1a7cb4a473f6
Arnold Waigwa <agw13@albion.edu>
2025-07-17 21:28:10 -0400

    created navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,17 +1,29 @@
 import Link from "next/link";
 import React from "react";
+import { IoIosBug } from "react-icons/io";
+
+//hover transitions for links
 
 const Navbar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "issue", href: "/Issue" },
+  ];
   return (
-    <nav className="flex bg-amber-900">
-      <Link href="/"> Logo </Link>
-      <ul className="flex">
-        <li>
-          <Link href="/"> Dashboard </Link>
-        </li>
-        <li>
-          <Link href="/issues">Issues</Link>
-        </li>
+    <nav className="flex space-x-6 border-b h-14 px-5 items-center mb-5 border-t my-1">
+      <Link href="/">
+        <IoIosBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className="text-zinc-500  hover:text-zinc-800 transition-colors"
+          >
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
