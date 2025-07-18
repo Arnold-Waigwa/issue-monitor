82e05455bc95e88262465f4f320aee1a6fd99140
Arnold Waigwa <agw13@albion.edu>
2025-07-18 19:37:07 -0400

    implemented active-link styling

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,14 +1,20 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import { IoIosBug } from "react-icons/io";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 //hover transitions for links
+//implement active link
 
 const Navbar = () => {
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issue", href: "/issue" },
   ];
+  const active_link = usePathname();
+
   return (
     <nav className="flex space-x-6 border-b h-14 px-5 items-center mb-5 border-t my-1">
       <Link href="/">
@@ -19,7 +25,11 @@ const Navbar = () => {
           <Link
             key={link.href}
             href={link.href}
-            className="text-zinc-500  hover:text-zinc-800 transition-colors"
+            className={classnames({
+              "text-zinc-900": link.href === active_link,
+              "text-zinc-500": link.href != active_link,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
           >
             {link.label}
           </Link>
