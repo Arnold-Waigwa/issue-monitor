4f14731dd31c120de38890f959c9cddc92ed32da
Arnold Waigwa <agw13@albion.edu>
2025-09-10 09:53:49 -0400

    style the issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -2,6 +2,8 @@ import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
 import React from "react";
 import delay from "delay";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 //calling the issues with Id
 interface Props {
   params: { id: string };
@@ -15,10 +17,14 @@ const page = async ({ params }: Props) => {
   await delay(2000);
   return (
     <div>
-      <p>{issues?.title}</p>
-      <p>{issues?.description}</p>
-      <p>{issues?.status}</p>
-      <p>{issues?.createdAt.toDateString()}</p>
+      <Heading>{issues?.title}</Heading>
+      <Flex gap="5" my="2">
+        <IssueStatusBadge status={issues.status} />
+        <Text>{issues?.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>
+        <p>{issues?.description}</p>
+      </Card>
     </div>
   );
 };
