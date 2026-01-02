7d6a5dfc695fdbd8e6762192b580aea8e5bf7458
Arnold Waigwa <agw13@albion.edu>
2026-01-02 18:45:54 -0500

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,31 +1,34 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import { Link, IssueStatusBadge } from "@/app/components";
+import NextLink from "next/link";
 import IssueActions from "./IssueActions";
 import { Issue, Status } from "@prisma/client";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
 
 interface Props {
   searchParams: { status: Status; orderBy: keyof Issue };
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
-  const statuses = Object.values(searchParams.status);
-
-  const status = statuses.includes(searchParams.status)
-    ? searchParams.status
-    : "";
-
-  const issues = await prisma.issue.findMany({
-    where: {
-      status: searchParams.status,
-    },
-  });
-
   const columns: { label: string; value: keyof Issue; className?: string }[] = [
     { label: "Issue", value: "title" },
     { label: "Status", value: "status", className: "hidden md:table-cell" },
     { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
   ];
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: {
+      status: searchParams.status,
+    },
+    orderBy,
+  });
 
   return (
     <div>
@@ -38,13 +41,16 @@ const IssuesPage = async ({ searchParams }: Props) => {
                 key={column.value}
                 className={column.className}
               >
-                <Link
+                <NextLink
                   href={{
                     query: { ...searchParams, orderBy: column.value },
                   }}
                 >
                   {column.label}
-                </Link>
+                  {column.value === searchParams.orderBy && (
+                    <ArrowUpIcon className="inline" />
+                  )}
+                </NextLink>
               </Table.ColumnHeaderCell>
             ))}
           </Table.Row>
